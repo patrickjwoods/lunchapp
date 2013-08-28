6815faa4e3f0134ef80eb66c83e4666c0893b7fc
Patrick Woods <patrickjwoods@gmail.com>
2013-08-27 22:46:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orderapp::Application.config.secret_key_base = '6ff9977126194f8baea5c0db4f28b4bd89b42251654485767104965b809fd7d747d4151d8a06a28e65b6e8dbad327d4e3ce02c04b151dccea6b057ea6a4340ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Orderapp::Application.config.session_store :cookie_store, key: '_orderapp_session'
